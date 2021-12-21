09ebfa35fd0b8565182508938f483e57bddb737d
mostaphaRoudsari <sadeghipour@gmail.com>
2021-12-21 15:07:21 -0500

    refactor(daylight factor app): refactored the UI

--- daylight-factor-app/app.py
+++ daylight-factor-app/app.py
@@ -1,8 +1,8 @@
 import json
 import pathlib
 import zipfile
-from typing import Any, Dict
 from uuid import uuid4
+from query import Query
 
 import streamlit as st
 from honeybee.model import Model, Room
@@ -40,106 +40,6 @@ api_key = st.sidebar.text_input(
     'generate one.'
 ) or None
 
-api_client = ApiClient(api_token=api_key)
-
-
-class Query:
-
-    _defaults = {
-        'width': 10,
-        'depth': 10,
-        'glazing_ratio': 0.4,
-        'job_id': None
-    }
-
-    def __init__(self):
-        self._query_params = st.experimental_get_query_params()
-        self._width = int(self._query_params.get('width', [10])[0])
-        self._depth = int(self._query_params.get('depth', [10])[0])
-        self._glazing_ratio = float(
-            self._query_params.get('glazing-ration', [0.4])[0])
-        self._job_id = self._query_params.get('job-id', [None])[0]
-        self._owner = self._query_params.get('owner', [None])[0]
-        self._project = self._query_params.get('project', [None])[0]
-        self.model_id = self._query_params.get('model-id', [str(uuid4())])[0]
-
-        self._update_query()
-
-    @property
-    def query_params(self):
-        params = {
-            'width': self._width,
-            'depth': self._depth,
-            'glazing-ratio': self._glazing_ratio,
-            'model-id': self.model_id
-        }
-
-        if self._job_id is not None:
-            params['job-id'] = self._job_id
-        if self._owner is not None:
-            params['owner'] = self._owner
-        if self._project is not None:
-            params['project'] = self._project
-
-        return params
-
-    def _update_query(self):
-        st.experimental_set_query_params(**self.query_params)
-
-    @property
-    def width(self):
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        self._width = value
-        self._update_query()
-
-    @property
-    def depth(self):
-        return self._depth
-
-    @depth.setter
-    def depth(self, value):
-        self._depth = value
-        self._update_query()
-
-    @property
-    def glazing_ratio(self):
-        return self._glazing_ratio
-
-    @glazing_ratio.setter
-    def glazing_ratio(self, value):
-        self._glazing_ratio = value
-        self._update_query()
-
-    @property
-    def job_id(self):
-        return self._job_id
-
-    @job_id.setter
-    def job_id(self, value):
-        self._job_id = value
-        self._update_query()
-
-    @property
-    def owner(self):
-        return self._owner
-
-    @owner.setter
-    def owner(self, value):
-        self._owner = value
-        self._update_query()
-
-    @property
-    def project(self):
-        return self._project
-
-    @project.setter
-    def project(self, value):
-        self._project = value
-        self._update_query()
-
 
 @st.cache(suppress_st_warning=True)
 def view_results(owner, project, job_id, api_key):
@@ -167,7 +67,7 @@ def view_results(owner, project, job_id, api_key):
                 "legend_parameters": {
                         "hide_legend": False,
                         "min": 0,
-                        "max": 20,
+                        "max": 10,
                         "color_set": "nuanced",
                         "label_parameters": {
                             "color": [34, 247, 10],
@@ -191,69 +91,70 @@ def view_results(owner, project, job_id, api_key):
 
 query = Query()
 
-with st.expander('Model Configuration'):
-    c1, c2, c3 = st.columns(3)
+query.owner = st.sidebar.text_input(
+    'Project Owner', value=query.owner
+)
 
-    query.width = c1.slider(
-        label='Width of the room', min_value=4, max_value=20,
-        value=query.width,
-    )
+query.project = st.sidebar.text_input(
+    'Project Name', value=query.project
+)
 
-    query.depth = c2.slider(
-        label='Depth of the room', min_value=4, max_value=20,
-        value=query.depth,
-    )
+run_simulation = st.sidebar.button('Run Simulation')
+api_client = ApiClient(api_token=api_key)
 
-    query.glazing_ratio = c3.slider(
-        label='Glazing ratio', min_value=0.1, max_value=0.9,
-        value=query.glazing_ratio,
-    )
-    room = Room.from_box(
-        identifier=str(uuid4()),
-        width=query.width,
-        depth=query.depth,
-    )
-    room.wall_apertures_by_ratio(query.glazing_ratio)
 
-    grid = SensorGrid.from_mesh3d(str(uuid4()), room.generate_grid(x_dim=0.5))
-    model = Model(identifier=query.model_id, rooms=[room])
-    model._properties._radiance = ModelRadianceProperties(model, [grid])
-    model_path = model.to_hbjson(name=model.identifier, folder='data')
-    vtk_path = VTKModel.from_hbjson(model_path, SensorGridOptions.Sensors).to_vtkjs(
-        folder='data', name=model.identifier)
+c1, c2, c3 = st.columns([1, 1, 2])
 
+query.width = c1.slider(
+    label='Width of the room', min_value=4, max_value=20,
+    value=query.width,
+)
+
+query.depth = c2.slider(
+    label='Depth of the room', min_value=4, max_value=20,
+    value=query.depth,
+)
+
+query.glazing_ratio = c1.slider(
+    label='Glazing ratio', min_value=0.1, max_value=0.9,
+    value=query.glazing_ratio,
+)
+room = Room.from_box(
+    identifier=str(uuid4()),
+    width=query.width,
+    depth=query.depth,
+)
+room.wall_apertures_by_ratio(query.glazing_ratio)
+
+grid = SensorGrid.from_mesh3d(str(uuid4()), room.generate_grid(x_dim=0.5))
+model = Model(identifier=query.model_id, rooms=[room])
+model._properties._radiance = ModelRadianceProperties(model, [grid])
+model_path = model.to_hbjson(name=model.identifier, folder='data')
+vtk_path = VTKModel.from_hbjson(model_path, SensorGridOptions.Sensors).to_vtkjs(
+    folder='data', name=model.identifier)
+
+with c3:
     st_vtkjs(
         pathlib.Path(vtk_path).read_bytes(),
         key=model.identifier
     )
 
-with st.expander('Simulation'):
 
-    c1, c2, c3 = st.columns(3)
+if run_simulation:
+    query.job_id = None
 
-    query.owner = c1.text_input(
-        'Project Owner', value=query.owner
-    )
+    recipe = Recipe('ladybug-tools', 'daylight-factor',
+                    'latest', api_client)
+    new_job = NewJob(query.owner, query.project, recipe, client=api_client)
+    model_project_path = new_job.upload_artifact(
+        pathlib.Path(model_path), 'streamlit-job')
+    new_job.arguments = [
+        {'width': query.width, 'depth': query.depth,
+            'glazing-ration': query.glazing_ratio, 'model': model_project_path}
+    ]
+    job = new_job.create()
 
-    query.project = c2.text_input(
-        'Project Name', value=query.project
-    )
-
-    if c3.button('Run Simulation'):
-        query.job_id = None
-
-        recipe = Recipe('ladybug-tools', 'daylight-factor',
-                        'latest', api_client)
-        new_job = NewJob(query.owner, query.project, recipe, client=api_client)
-        model_project_path = new_job.upload_artifact(
-            pathlib.Path(model_path), 'streamlit-job')
-        new_job.arguments = [
-            {'width': query.width, 'depth': query.depth,
-                'glazing-ration': query.glazing_ratio, 'model': model_project_path}
-        ]
-        job = new_job.create()
-
-        query.job_id = job.id
+    query.job_id = job.id
 
     if query.job_id is not None and query.owner is not None and query.project is not None:
 
