00756d37dc29c0233a1a299aefd77a60d0301cec
Devang <devngc@gmail.com>
2022-01-20 18:22:20 +0530

    feat(app.py): add PDF export

--- outdoor-comfort/app.py
+++ outdoor-comfort/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import shutil
 import base64
 
-from fpdf import FPDF as PDF
+from fpdf import FPDF
 from plotly.graph_objects import Figure
 from typing import List
 
@@ -29,8 +29,11 @@ st.sidebar.image(
 
 
 def main():
-    with st.sidebar:
 
+    ####################################################################################
+    # sidebar
+    ####################################################################################
+    with st.sidebar:
         # epw file
         with st.expander('Upload an EPW file'):
             epw_data = st.file_uploader('', type='epw')
@@ -109,10 +112,10 @@ def main():
 
         # analysis type
         anlysis_type = st.radio('Analysis type', options=[
-            'utci', 'comfort', 'condition', 'category'])
+            'UTCI', 'Comfortable or not', 'Comfort conditions', 'Comfort categories'])
 
         # scenarios
-        wind_help = 'Select "Add wind" include the EPW wind speed in the calculation.'\
+        wind_help = 'Select "Add wind" to include the EPW wind speed in the calculation.'\
             ' Not selecting this will assume a condition that is shielded from wind'\
             ' where the human subject experiences a low wind speed of 0.5 m/s, which'\
             ' is the lowest input speed that is recommended for the UTCI model. \n'\
@@ -133,7 +136,9 @@ def main():
                                                   'Add wind & sun'],
                             help=wind_help + '\n' + sun_help + '\n' + win_sun_help)
 
-    # selecting scenarios to plot
+    ####################################################################################
+    # helper functions and info
+    ####################################################################################
     if scenario == 'Add wind & sun':
         comf_objs = [
             UTCI.from_epw(epw, include_wind=False, include_sun=False),
@@ -169,11 +174,8 @@ def main():
         col1, col2 = st.columns([5, 1])
 
         with col1:
-
             # figure title
             title = f'{scenario}'
-            titles.append(title)
-
             if analysis_period:
                 hourly_data = hourly_data.filter_by_analysis_period(analysis_period)
 
@@ -189,10 +191,14 @@ def main():
                                           colors=colors, min_range=min_range,
                                           max_range=max_range, num_labels=num_labels,
                                           labels=labels)
-            figures.append(figure)
+
             st.plotly_chart(figure, use_container_width=True)
+            figures.append(figure)
 
         with col2:
+            num_hours = len(hourly_data.values)
+            percent_comfortable = round(percent_comfortable, 2)
+
             title = ("<div><br>" +
                      "<br>" +
                      "<br>" +
@@ -200,11 +206,14 @@ def main():
                      "<br>" +
                      "<br>" +
                      " <h3 style ='text-align: center; padding-bottom:0px;" +
-                     f" color: gray;'>{round(percent_comfortable, 2)} %</h3>" +
+                     f" color: gray;'>{percent_comfortable} %</h3>" +
                      "<p style ='text-align: center;" +
-                     f" color: gray;'>comfortable in {len(hourly_data.values)} hours</div>")
+                     f" color: gray;'>comfortable in {num_hours} hours</div>")
 
             st.markdown(title, unsafe_allow_html=True)
+
+            percentages.append(
+                f'{percent_comfortable}% comfortable in {num_hours} hours')
 
     title_scenario = {
         0: ' without the effect of sun and wind',
@@ -213,32 +222,38 @@ def main():
         3: ' with the effect of sun ☀️ and wind 💨'
     }
 
-    figures, titles = [], []
+    figures, percentages = [], []
+
+    ####################################################################################
     # Main page
+    ####################################################################################
     with st.container():
 
         # Creating face columns to display button at the center
         col1 = st.columns(3)[1]
         with col1:
-            st.header('Outdoor-comfort')
+            st.subheader(epw.location.city)
+
+        intro_1 = 'Calculate the Universal Thermal Climate Index (UTCI)'\
+            ' for a set of input climate conditions. Perhaps the most familiar'\
+            ' application of Universal Thermal Climate Index(UTCI) is the'\
+            ' temperature given by TV weathermen and women when they say that,'\
+            ' "even though the dry bulb temperature outside is a certain value,'\
+            ' the temperature actually "feels like" something higher or lower. \n'
+
+        intro_2 = 'UTCI is this temperature of what the weather "feels like" and it'\
+            ' takes into account the radiant temperature(sometimes including'\
+            ' solar radiation) , relative humidity, and wind speed.'\
+            ' UTCI uses these variables in a human energy balance model to'\
+            ' give a temperature value that is indicative of the heat stress'\
+            ' or cold stress felt by a human body outdoors.'
 
         # page header
-        st.markdown('Use this app to calculate the Universal Thermal Climate Index (UTCI)'
-                    ' for a set of input climate conditions. Perhaps the most familiar'
-                    ' application of Universal Thermal Climate Index(UTCI) is the'
-                    ' temperature given by TV weathermen and women when they say that,'
-                    ' "even though the dry bulb temperature outside is a certain value,'
-                    ' the temperature actually "feels like" something higher or lower.')
+        st.markdown(intro_1)
+        st.markdown(intro_2)
 
-        st.markdown('UTCI is this temperature of what the weather "feels like" and it'
-                    ' takes into account the radiant temperature(sometimes including'
-                    ' solar radiation) , relative humidity, and wind speed.'
-                    ' UTCI uses these variables in a human energy balance model to'
-                    ' give a temperature value that is indicative of the heat stress'
-                    ' or cold stress felt by a human body outdoors')
-
-        if anlysis_type == 'utci':
-            st.subheader(f'UTCI in Celsius for {epw.location.city}')
+        if anlysis_type == 'UTCI':
+            st.markdown('UTCI in Celsius')
             # get min and max of first hourly data and apply it to all to keep
             # consistent legend for comparison
             min = comf_objs[0].universal_thermal_climate_index.min
@@ -246,49 +261,52 @@ def main():
             # TODO add legend labels as per number of labels
             for count, obj in enumerate(comf_objs):
                 plot_figure_with_title(obj.universal_thermal_climate_index,
-                                       title_scenario[count],
+                                       'UTCI ' + title_scenario[count],
                                        obj.percent_comfortable,
                                        analysis_period=lb_ap,
                                        conditional_statement=conditional_statement,
                                        min_range=min, max_range=max, colors=colorset)
 
-        elif anlysis_type == 'comfort':
-            st.subheader(f'Comfortable or not for {epw.location.city}')
+        elif anlysis_type == 'Comfortable or not':
             st.markdown(
                 """
+                Comfortable or not
+
                 -   0 = Uncomfortable (thermal stress)
                 -   1 = Comfortable (no thermal stress)
                 """)
             for count, obj in enumerate(comf_objs):
                 plot_figure_with_title(obj.is_comfortable,
-                                       title_scenario[count],
+                                       'Comfortable or not ' + title_scenario[count],
                                        obj.percent_comfortable,
                                        analysis_period=lb_ap,
                                        conditional_statement=conditional_statement,
                                        min_range=0, max_range=1,
                                        num_labels=2, labels=[0, 1], colors=colorset)
 
-        elif anlysis_type == 'condition':
-            st.subheader(f'Comfort conditions for {epw.location.city}')
+        elif anlysis_type == 'Comfort conditions':
             st.markdown(
                 """
+                Comfort conditions
+
                 -   -1 = Cold
                 -    0 = Neutral
                 -   +1 = Hot
                 """)
             for count, obj in enumerate(comf_objs):
                 plot_figure_with_title(obj.thermal_condition,
-                                       title_scenario[count],
+                                       'Comfort conditions' + title_scenario[count],
                                        obj.percent_comfortable,
                                        analysis_period=lb_ap,
                                        conditional_statement=conditional_statement,
                                        min_range=-1, max_range=1,
                                        num_labels=3, labels=[-1, 0, 1], colors=colorset)
 
-        elif anlysis_type == 'category':
-            st.subheader(f'Comfort categories for {epw.location.city}')
+        elif anlysis_type == 'Comfort categories':
             st.markdown(
                 """
+                Comfort categories
+
                 -   -5 = Extreme Cold Stress       (UTCI < -40)
                 -   -4 = Very Strong Cold Stress   (-40 <= UTCI < -27)
                 -   -3 = Strong Cold Stress        (-27 <= UTCI < -13)
@@ -303,7 +321,7 @@ def main():
                 """)
             for count, obj in enumerate(comf_objs):
                 plot_figure_with_title(obj.thermal_condition_eleven_point,
-                                       title_scenario[count],
+                                       'Comfort categories ' + title_scenario[count],
                                        obj.percent_comfortable,
                                        analysis_period=lb_ap,
                                        conditional_statement=conditional_statement,
@@ -317,44 +335,118 @@ def main():
     ####################################################################################
 
     def create_download_link(val, filename):
+        """Create download link for the pdf.
+
+        Args:
+            val: Bytes
+            filename: String
+
+        Returns
+            A download link
+        """
+
         b64 = base64.b64encode(val)  # val looks like b'...'
         return '<a style="display:block; text-align: left" '\
             f' href="data:application/octet-stream;base64,{b64.decode()}" '\
             f' download="{filename}.pdf">Download PDF</a>'
 
-    def write_pdf(col):
+    def write_pdf(col: st.columns):
         # Create a folder to write images
+        path = pathlib.Path('./assets/images/figures')
+
         # nuke the images folder if exists
-        path = pathlib.Path('./assets/figures')
         if path.is_dir():
             shutil.rmtree(path)
+
         # create a new folder to download the image
         path.mkdir(parents=True, exist_ok=True)
 
+        class PDF(FPDF):
+            def header(self):
+                # Logo
+                self.image('./assets/images/pollination_brandmark.png', 130, 8, 33)
+
+            # Page footer
+            def footer(self):
+                # Position at 1.5 cm from bottom
+                self.set_y(-15)
+                # Arial italic 8
+                self.set_font('Arial', 'I', 8)
+                # Page number
+                self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
+
+            def chater_title(self, label):
+                self.set_font('Arial', 'B', 14)
+                # Title
+                self.cell(0, 6, f'{label}', 0, 1, 'L', 0)
+                # Line break
+                self.ln(4)
+
+            def chapter_body(self, text):
+                self.set_font('Arial', '', 14)
+                # Output justified text
+                self.multi_cell(0, 5, text)
+                # Line break
+                self.ln()
+
         # instantiate the report
         pdf = PDF(orientation='L', unit='mm', format='A4')
+        # pdf.set_margins(right=50, top=50, left=50)
+        # pdf.set_auto_page_break(True, margin=50)
 
         # image type
         image_format = 'jpg'
-        # container width & height
 
+        # container width & height
         # A little less than the width of A4 paper in mm
         container_width = 280
+        container_height = container_width / 1.414
 
         # add front page
         pdf.add_page()
-        pdf.set_font('Arial', 'B', 20)
-        pdf.cell(container_width, 170, epw.location.city, align='C')
+        pdf.set_font('Arial', 'B', 16)
+        pdf.cell(container_width, 170,
+                 f'Outdoor Comfort Report | {epw.location.city}', align='C')
+
+        # add summary page
+        pdf.add_page()
+        pdf.chater_title('Introduction')
+        pdf.chapter_body(intro_1 + '\n' + intro_2)
+
+        # add parameters page
+        pdf.add_page()
+        pdf.chater_title('Parameters')
+
+        # collect all the parameters
+        location_par = f'EPW: country: {epw.location.country}, state: {epw.location.state},'\
+            f' city: {epw.location.city}, time zone: {epw.location.time_zone} \n'
+
+        ap = lb_ap if lb_ap else AnalysisPeriod()
+        analysis_period_par = f'Analysis period: start month{ap.st_month}, start day: {ap.st_day}, '\
+            f'start hour: {ap.st_hour}, end month: {ap.end_month}, end day: {ap.end_day}, \n '
+        conditional_par = f'Conditional statement: '\
+            f' {conditional_statement if conditional_statement else "Not set"} \n'
+        analysis_type_par = f'Analysis type: {anlysis_type} \n'
+        scenario_par = f'Scenario: {scenario} \n'
+        # write parameters
+        pdf.chapter_body(location_par + '\n' + analysis_period_par + '\n' +
+                         conditional_par + '\n' + analysis_type_par + '\n' + scenario_par)
 
         # add other charts
         for count, figure in enumerate(figures):
             pdf.add_page()
-            pdf.set_font('Arial', 'B', 16)
-            # pdf.cell(container_width, 10, header, align='C')
+            pdf.set_font('Arial', '', 14)
             image_path = path.joinpath(str(count) + '.' + image_format)
-            figure.write_image(image_path, format=image_format, scale=4)
-            pdf.image(image_path.as_posix(), x=10, y=20,
-                      w=container_width)
+            # set black font color for the text on the image since fpdf does not
+            # support setting text color
+            figure.update_layout(
+                font=dict(color='#000000')
+            )
+            figure.write_image(image_path, format=image_format,
+                               width=500, height=250, scale=3)
+            pdf.image(image_path.as_posix(), x=(297/2 - 150/2), y=60, w=150, h=75)
+            pdf.cell(w=container_width, h=50,
+                     txt=percentages[count], align='C')
 
         html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
         with col:
