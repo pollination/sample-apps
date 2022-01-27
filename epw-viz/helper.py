0398ea6fb24fcfe1e3a6596c5494b31137681c52
Devang <devngc@gmail.com>
2022-01-27 16:38:38 +0530

    refactor(*): refactor hourly_data and bar_chart

--- epw-viz/helper.py
+++ epw-viz/helper.py
@@ -4,16 +4,84 @@ import pathlib
 import shutil
 import streamlit as st
 
+from typing import List, Tuple
 from icrawler.builtin import GoogleImageCrawler
 from geopy.geocoders import Nominatim
+from plotly.graph_objects import Figure
+from ladybug.datacollection import HourlyContinuousCollection
+
+from ladybug.epw import EPW, EPWFields
+from ladybug.color import Colorset, Color
+from ladybug.legend import LegendParameters
+from ladybug.hourlyplot import HourlyPlot
+from ladybug.monthlychart import MonthlyChart
+
+colorsets = {
+    'original': Colorset.original(),
+    'nuanced': Colorset.nuanced(),
+    'annual_comfort': Colorset.annual_comfort(),
+    'benefit': Colorset.benefit(),
+    'benefit_harm': Colorset.benefit_harm(),
+    'black_to_white': Colorset.black_to_white(),
+    'blue_green_red': Colorset.blue_green_red(),
+    'cloud_cover': Colorset.cloud_cover(),
+    'cold_sensation': Colorset.cold_sensation(),
+    'ecotect': Colorset.ecotect(),
+    'energy_balance': Colorset.energy_balance(),
+    'energy_balance_storage': Colorset.energy_balance_storage(),
+    'glare_study': Colorset.glare_study(),
+    'harm': Colorset.harm(),
+    'heat_sensation': Colorset.heat_sensation(),
+    'multi_colored': Colorset.multi_colored(),
+    'multicolored_2': Colorset.multicolored_2(),
+    'multicolored_3': Colorset.multicolored_3(),
+    'openstudio_palette': Colorset.openstudio_palette(),
+    'peak_load_balance': Colorset.peak_load_balance(),
+    'shade_benefit': Colorset.shade_benefit(),
+    'shade_benefit_harm': Colorset.shade_benefit_harm(),
+    'shade_harm': Colorset.shade_harm(),
+    'shadow_study': Colorset.shadow_study(),
+    'therm': Colorset.therm(),
+    'thermal_comfort': Colorset.thermal_comfort(),
+    'view_study': Colorset.view_study()
+}
 
 
-@st.cache(suppress_st_warning=True)
-def get_image(keyword):
+def epw_hash_func(epw: EPW) -> str:
+    """Function to help streamlit hash an EPW object."""
+    return epw.location.city
+
+
+def hourly_data_hash_func(hourly_data: HourlyContinuousCollection) -> str:
+    """Function to help streamlit hash an HourlyContinuousCollection object."""
+    return hourly_data.header.data_type
+
+
+def color_hash_func(color: Color) -> Tuple[float, float, float]:
+    """Function to help streamlit hash a Color object."""
+    return color.r, color.g, color.b
+
+
+@st.cache()
+def get_fields() -> dict:
+    # A dictionary of EPW variable name to its corresponding field number
+    return {EPWFields._fields[i]['name'].name: i for i in range(6, 34)}
+
+
+@st.cache()
+def get_image(latitude: float, longitude: float) -> None:
+    # find city name and create a keyword for search
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.reverse(str(latitude)+","+str(longitude), language='en')
+    address = location.raw['address']
+    city = address.get('city', '')
+    keyword = city + ' city image'
+
     # nuke the images folder if exists
     path = pathlib.Path('./assets/image')
     if path.is_dir():
         shutil.rmtree(path)
+
     # create a new folder to download the image
     path.mkdir(parents=True, exist_ok=True)
     filters = dict(size='medium', type='photo',
@@ -22,10 +90,82 @@ def get_image(keyword):
     google_crawler.crawl(keyword=keyword, max_num=1, filters=filters)
 
 
-def city_name(latitude: float, longitude: float) -> str:
-    """Get the city name from latitude and longitude"""
-    geolocator = Nominatim(user_agent="geoapiExercises")
-    location = geolocator.reverse(str(latitude)+","+str(longitude), language='en')
-    address = location.raw['address']
-    city = address.get('city', '')
-    return city
+@st.cache(allow_output_mutation=True, hash_funcs={HourlyContinuousCollection: hourly_data_hash_func, Color: color_hash_func})
+def get_hourly_data_figure(data: HourlyContinuousCollection, global_colorset: str,
+                           conditional_statement: str, min: float, max: float) -> Figure:
+    """Function to help streamlit create a plotly figure from a HourlyContinuousCollection object.
+
+    Args:
+        data: HourlyContinuousCollection object.
+        global_colorset: A string representing the name of a Colorset.
+        conditional_statement: A string representing a conditional statement.
+        min: A string representing the lower bound of the data range.
+        max: A string representing the upper bound of the data range.
+
+    Returns:
+        A plotly figure.
+    """
+
+    if conditional_statement:
+        try:
+            data = data.filter_by_conditional_statement(
+                conditional_statement)
+        except AssertionError:
+            st.error('No values found for that conditional statement')
+            data = data
+        except ValueError:
+            st.error('Invalid conditional statement')
+            data = data
+
+    if min:
+        try:
+            min = float(min)
+        except ValueError:
+            st.error('Invalid minimum value')
+            min = None
+
+    if max:
+        try:
+            max = float(max)
+        except ValueError:
+            st.error('Invalid maximum value')
+            max = None
+
+    lb_lp = LegendParameters(colors=colorsets[global_colorset])
+
+    if min:
+        lb_lp.min = min
+    if max:
+        lb_lp.max = max
+
+    hourly_plot = HourlyPlot(data, legend_parameters=lb_lp)
+
+    return hourly_plot.plot(title=str(data.header.data_type), show_title=True)
+
+
+@st.cache(hash_funcs={EPW: epw_hash_func, Color: color_hash_func})
+def get_bar_chart_figure(fields: dict, epw: EPW, selection: List[str], data_type: str,
+                         switch: bool, stack: bool, global_colorset: str) -> Figure:
+
+    if switch:
+        colors = list(colorsets[global_colorset])
+        colors.reverse()
+    else:
+        colors = colorsets[global_colorset]
+
+    data = []
+    for count, item in enumerate(selection):
+        if item:
+            var = epw._get_data_by_field(fields[list(fields.keys())[count]])
+            if data_type == 'Monthly average':
+                data.append(var.average_monthly())
+            elif data_type == 'Monthly total':
+                data.append(var.total_monthly())
+            elif data_type == 'Daily average':
+                data.append(var.average_daily())
+            elif data_type == 'Daily total':
+                data.append(var.total_daily())
+
+    lb_lp = LegendParameters(colors=colors)
+    monthly_chart = MonthlyChart(data, legend_parameters=lb_lp)
+    return monthly_chart.plot(stack=stack, title=data_type, show_title=True)
